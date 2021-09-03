d11514cf5fb2e5c45eeba952b80c50aa6832a247
dranzzer <89338308+dranzzer@users.noreply.github.com>
2021-09-03 15:48:38 +0530

    getting db scheme

---
+++ sql_scripts.sql
@@ -0,0 +1,10 @@
+
+-- see initial outlay of databases
+show databases
+
+-- checking if database from previous testing
+drop database if exists python_demo
+
+
+-- creating new db
+create database python_demo

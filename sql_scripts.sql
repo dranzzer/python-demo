d79c0b76437e5cf7a3946c7a0db5aba631b79dee
dranzzer <89338308+dranzzer@users.noreply.github.com>
2021-09-03 16:54:05 +0530

    mysql workbench taking too long. changing to sql studio

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -15,5 +15,16 @@ create database python_demo
 use python_demo;
 create table covid(
 tag varchar(10),
-continent var
+continent varchar(50),
+location varchar(50),
+date date,
+population int,
+total_cases int,
+new_cases int,
+new_cases_smoothed float,
+total_deaths int,
+new_deaths int,
+new_deaths_smoothed float)
+
+select * from covid
 

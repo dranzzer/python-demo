38d87905b7a02755f9c44e91289a834893b22bdd
dranzzer <89338308+dranzzer@users.noreply.github.com>
2021-09-05 16:25:24 +0530

    initial calendar build

--- gui_pilot.py
+++ gui_pilot.py
@@ -1,4 +1,23 @@
 from tkinter import *
+from tkcalendar import *
 
 
-main_window = Tk()
+
+root = Tk()
+root.title('Generator')
+root.geometry("600x400")
+
+cal1 = Calendar (root, selectmode = "day", year=2020, month=5, day=22)
+cal1.pack(pady=20)
+
+
+def grab_date():
+    label.config(text=cal1.get_date())
+
+button = Button(root,text="get date",command=grab_date)
+button.pack(pady=20)
+
+label = Label(root,text="")
+label.pack(pady=20)
+
+root.mainloop()

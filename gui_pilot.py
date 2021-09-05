f454ceb5c6eb1b0c0107bd0d4b9a3b45d0131f08
dranzzer <89338308+dranzzer@users.noreply.github.com>
2021-09-05 17:02:53 +0530

    implemetn UI grid

--- gui_pilot.py
+++ gui_pilot.py
@@ -3,21 +3,30 @@ from tkcalendar import *
 
 
 
-root = Tk()
-root.title('Generator')
-root.geometry("600x400")
+window = Tk()
+window.title('Generator')
+window.geometry("650x400")
 
-cal1 = Calendar (root, selectmode = "day", year=2020, month=5, day=22)
-cal1.pack(pady=20)
+cal1 = Calendar(window, selectmode = "day", year=2020, month=5, day=22)
+cal2 = Calendar(window, selectmode = "day", year=2020, month=5, day=22)
+cal1.grid(row=1,column=0)
+cal2.grid(row=1,column=2)
+
 
 
 def grab_date():
     label.config(text=cal1.get_date())
 
-button = Button(root,text="get date",command=grab_date)
-button.pack(pady=20)
+button = Button(window,text="get date",command=grab_date)
+button.grid(row=3,column=1)
 
-label = Label(root,text="")
-label.pack(pady=20)
+label = Label(window,text="")
+label.grid(row=4,column=2)
+Label(window,text="Start Date :").grid(row=0,column=0)
+Label(window,text="Start Date :").grid(row=0,column=2)
 
-root.mainloop()
+
+
+
+window.resizable(False,False)
+window.mainloop()
